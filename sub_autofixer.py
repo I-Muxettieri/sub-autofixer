3ad1f4bd2ef2501d903253be343c203aeefecdf9
Ghegghe <nicopante98@gmail.com>
2024-08-06 15:02:45 +0200

    restyler dialogue lines

--- sub_autofixer.py
+++ sub_autofixer.py
@@ -1,26 +1,57 @@
-from typing import List, Optional
-import pysubs2
+import re
+from typing import Dict, List, Optional
+from pysubs2 import SSAEvent, SSAStyle, load as load_sub
 import argparse
 import os
 import subprocess
+from utils import parse_style
 
-
-def resample_script(sub: str, video: str, output: str):
-    new_filename = os.path.join(output, os.path.splitext(os.path.basename(sub))[0])
-    command = f'aegisub-cli --loglevel 4 --video "{video}" "{sub}" "{new_filename}.ass" "tool/resampleres"'
-    subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+TYPESETTING_TAGS_REGEX = r"\\pos|\\move"
+OVERRIDE_TAGS_REGEX = r"{\\[^}]+}"
 
 
 def is_valid_sub_file(filename: str) -> bool:
     return filename.endswith(".ass") or filename.endswith(".srt")
 
 
-def sub_converter(filename: str, output: str):
+def sub_converter(filename: str, output: str) -> str:
     name = os.path.splitext(os.path.basename(filename))
     new_path = os.path.join(output, f"{name[0]}.ass")
-    subs = pysubs2.load(filename)
+    subs = load_sub(filename)
     subs.save(new_path, format_="ass")
     return new_path
+
+
+def resample_script(sub: str, video: str, output: str):
+    new_filename = os.path.join(output, os.path.splitext(os.path.basename(sub))[0])
+    aegi_resample = f'aegisub-cli --loglevel 4 --video "{video}" "{sub}" "{new_filename}.ass" "tool/resampleres"'
+    # archi_resample = f'aegisub-cli --loglevel 4 --video "{video}" "{new_filename}" "{new_filename}" "tool/resampleres"'
+    subprocess.run(
+        aegi_resample, shell=True, check=True, capture_output=True, text=True
+    )
+    # subprocess.run(
+    #     archi_resample, shell=True, check=True, capture_output=True, text=True
+    # )
+
+
+def is_dialogue_line(line: SSAEvent) -> bool:
+    tags: List[str] = re.findall(OVERRIDE_TAGS_REGEX, line.text)
+    for tag in tags:
+        if re.search(TYPESETTING_TAGS_REGEX, tag):
+            return False
+    return True
+
+
+def restyle_dialogue(filename: str, output: str, restyling_styles: Dict[str, SSAStyle]):
+    subs = load_sub(filename)
+
+    subs.styles.update(restyling_styles)
+
+    for line in subs:
+        if is_dialogue_line(line):
+            line.style = list(restyling_styles.keys())[0]
+
+    subs.save(os.path.join(output, os.path.basename(filename)), format_="ass")
 
 
 def main():
@@ -52,14 +83,21 @@ def main():
     else:
         raise Exception("Sub file err")
 
+    restyling_styles = {}
+    with open("styles.txt", "r") as styles:
+        for style in styles:
+            restyling_styles.update(parse_style(style.strip()))
+
     for filename in filenames:
         print(f"Fixing: {filename}")
 
-        if not filename.endswith(".ass"):
-            filename = sub_converter(filename, output)
+        filename = sub_converter(filename, output)
 
         if video:
             resample_script(filename, video, output)
+
+        if len(restyling_styles):
+            restyle_dialogue(filename, output, restyling_styles)
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,55 @@
+from typing import Dict
+from pysubs2 import Color, Alignment, SSAStyle
+
+
+def parse_color(color: str) -> Color:
+    parsed_color = color
+    if parsed_color.startswith("&H"):
+        parsed_color = color[2:]
+    elif parsed_color.startswith("H"):
+        parsed_color = color[1:]
+    if parsed_color.endswith("H"):
+        parsed_color = parsed_color[:1]
+
+    red = int(parsed_color[4:6], 16)
+    green = int(parsed_color[2:4], 16)
+    blue = int(parsed_color[:2], 16)
+    alpha = 0
+    if len(parsed_color) > 6:
+        alpha = int(parsed_color[6:8], 16)
+
+    return Color(red, green, blue, alpha)
+
+
+def parse_style(style: str) -> Dict[str, SSAStyle]:
+    if not style.startswith("Style: "):
+        return {}
+    style = style[7:]
+    parts = style.split(",")
+
+    parsed_style = SSAStyle()
+
+    parsed_style.fontname = parts[1]
+    parsed_style.fontsize = float(parts[2])
+    parsed_style.primarycolor = parse_color(parts[3])
+    parsed_style.secondarycolor = parse_color(parts[4])
+    parsed_style.outlinecolor = parse_color(parts[5])
+    parsed_style.backcolor = parse_color(parts[6])
+    parsed_style.bold = bool(parts[7])
+    parsed_style.italic = bool(parts[8])
+    parsed_style.underline = bool(parts[9])
+    parsed_style.strikeout = bool(parts[10])
+    parsed_style.scalex = float(parts[11])
+    parsed_style.scaley = float(parts[12])
+    parsed_style.spacing = float(parts[13])
+    parsed_style.angle = float(parts[14])
+    parsed_style.borderstyle = int(parts[15])
+    parsed_style.outline = float(parts[16])
+    parsed_style.shadow = float(parts[17])
+    parsed_style.alignment = Alignment(int(parts[18]))
+    parsed_style.marginl = int(parts[19])
+    parsed_style.marginr = int(parts[20])
+    parsed_style.marginv = int(parts[21])
+    parsed_style.encoding = int(parts[22])
+
+    return {parts[0]: parsed_style}
